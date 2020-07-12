47c287c9b4c524839b9475498f205e43d73278a4
guillermo1974 <67216086+guillermo1974@users.noreply.github.com>
2020-07-12 14:35:05 -0400

    Update 03-install.R

--- scripts/tema1/03-install.R
+++ scripts/tema1/03-install.R
@@ -1,4 +1,10 @@
---instalar paquetes
+# Instalar paquetes
+
+## Comandos para instalar paquetes:
+
+# install.packages("nombre paquete", dep=TRUE) : instala el paquete, el dep=TRUE carga todas la dependencias necesarias
+# library(nombre paquete) : carga el paquete
+# cuando cierro una sesion todos los paquetes se cierran por lo tanto es necesario cargar de nuevo el paquete
 
 install.packages("tidyverse", dep = TRUE)
 
@@ -8,6 +14,10 @@ install.packages("magic", dep = TRUE)
 
 library(magic)
 
+install.packages("abind", dep=TRUE)
+
 magic(6)
+
+# installed.packages() : lista todos los paquetes instalados
 
 installed.packages()
