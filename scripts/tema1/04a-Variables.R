29381af4d68d8ce7c4b14417e70343fd8bea9f6c
guillermo1974 <67216086+guillermo1974@users.noreply.github.com>
2020-07-12 23:30:03 -0400

    Update 04a-Variables.R

--- scripts/tema1/04a-Variables.R
+++ scripts/tema1/04a-Variables.R
@@ -8,6 +8,8 @@
 
 # valor asignado a la variable <-  nombre variable
 
+# Ejemplos
+
 
 x = (pi^2)/2
 x
@@ -33,92 +35,3 @@ x = x^2
 
 x = sqrt(x)
 
-## Función f(x) = x^3 - (3^x)* sen(x)
-f = function(x) {
-  x^3 - (3^x) * sin(x)
-}
-
-f(4) # 4^3 - 3^4 * sin(4)
-f(5)
-f(pi/2)
-
-suma1 <- function(t){
-  t + 1
-}
-suma1(6)
-suma1(-5)
-
-product <- function(x, y){
-  x*y
-}
-product(5,7)
-
-g <- function(x,y,z) {
-  exp(x^2 + y^2) * sin(z)
-}
-g(1, 2, 3)
-g(1, -1, pi)
-
-suma5 <- function(numero){
-  numero = suma1(numero);
-  numero = suma1(numero);
-  numero = suma1(numero);
-  numero = suma1(numero);
-  suma1(numero)
-}
-
-suma5(3)
-
-ls()
-
-rm(product)
-
-ls()
-
-rm(list = ls())
-
-class(3+2i)
-
-(3+2i)*5
-
-(3+2i)*(-1+3i)
-
-(3+2i)/(-1+3i)
-
-#Esto es un error:
-#2+7*i
-
-#pi + sqrt(2)i
-complex(real = pi, imaginary = sqrt(2)) -> z1
-z1
-
-sqrt(-5)
-sqrt(as.complex(-5))
-
-#La raíz cuadrada devuelve, de las dos soluciones la de
-#Re(z)>0, para obtener la otra,  hay que multiplicar por -1
-sqrt(3+2i) # z^2 = 3+2i
-exp(3+2i)
-sin(3+2i)
-cos(3+2i)
-
-#Módulo = sqrt(Re(z)^2 + Im(z)^2)
-Mod(z1)
-#Argumento = arctan(Im(z)/Re(z))
-# = arccos(Re(z)/Mod(z))
-# = arcsin(Im(z)/Mod(z))
-# va de (-pi, pi]
-Arg(-1+0i)
-Arg(-1-2i)
-#Conjugado = Re(z)- Im(z)i
-Conj(z1)
-#Parte Real y Parte Imaginaria
-Re(z1)
-Im(z1)
-
-### z = Mod(z) * (cos(Arg(z))+sin(Arg(z))i)
-complex(modulus = 2, argument = pi/2) -> z2
-z2
-Mod(z2)
-Arg(z2)
-pi/2
